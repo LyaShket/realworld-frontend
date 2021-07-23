086f5da38c0824ec74d09b2015e929f9659a498e
Dmytrij Demyanenko <dingofleep@gmail.com>
2021-07-23 15:59:36 +0300

    feat: add Home view

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import AppRegister from "@/views/Register";
+import AppHome from "@/views/Home";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: AppHome
+  },
   {
     path: "/register",
     name: "register",
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h2>Hello, world!</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppHome"
+};
+</script>
