854a22db20e4d0f1c85c28ef4e5c6a65e6e0d661
Dmytrij Demyanenko <dingofleep@gmail.com>
2021-07-24 11:22:55 +0300

    feat: add active classes for all router-view in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,13 +20,21 @@
             </li>
 
             <li class="nav-item">
-              <router-link class="nav-link" :to="{ name: 'editor' }">
+              <router-link
+                class="nav-link"
+                :to="{ name: 'editor' }"
+                active-class="active"
+              >
                 <i class="ion-compose"></i>&nbsp;New Article
               </router-link>
             </li>
 
             <li class="nav-item">
-              <router-link class="nav-link" :to="{ name: 'settings' }">
+              <router-link
+                class="nav-link"
+                :to="{ name: 'settings' }"
+                active-class="active"
+              >
                 <i class="ion-gear-a"></i>&nbsp;Settings
               </router-link>
             </li>
@@ -38,6 +46,7 @@
                   name: 'user',
                   params: { username: currentUser.username }
                 }"
+                active-class="active"
               >
                 <img class="user-pic" :src="currentUser.image" />
                 {{ currentUser.username }}
