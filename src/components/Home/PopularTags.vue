151dffafbf254e03eabd4275b4602d6b25caa09e
Dmytrij Demyanenko <dingofleep@gmail.com>
2021-07-24 17:23:16 +0300

    feat: add a lot of logic for PopularTags

--- src/components/Home/PopularTags.vue
+++ src/components/Home/PopularTags.vue
@@ -3,20 +3,20 @@
     <div class="sidebar">
       <p>Popular Tags</p>
 
-      <div class="tag-list" ng-show="$ctrl.tags">
-        <a href="" class="tag-default tag-pill ng-binding ng-scope">
-          dragons
-        </a>
-        <a href="" class="tag-default tag-pill ng-binding ng-scope">
-          butt
+      <div class="tag-list" v-if="tags && tags.length > 0">
+        <a
+          href=""
+          class="tag-default tag-pill ng-binding ng-scope"
+          v-for="tag in tags"
+          :key="tag"
+        >
+          {{ tag }}
         </a>
       </div>
-
-      <div class="ng-hide">
+      <div class="ng-hide" v-else-if="!tags">
         Loading tags...
       </div>
-
-      <div class="post-preview ng-hide">
+      <div class="post-preview ng-hide" v-else>
         No tags are here... yet.
       </div>
     </div>
@@ -24,8 +24,21 @@
 </template>
 
 <script>
+import axios from "@/api/axios";
+
 export default {
-  name: "AppPopularTags"
+  name: "AppPopularTags",
+  data() {
+    return {
+      tags: null
+    };
+  },
+  created() {
+    axios.get("tags").then(response => {
+      this.isLoading = true;
+      this.tags = response.data.tags;
+    });
+  }
 };
 </script>
 
